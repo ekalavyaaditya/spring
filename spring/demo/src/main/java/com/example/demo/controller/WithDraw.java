f5618819abcdb55221b16bc465d245320cca2e70
aditya <129822187+ekalavyaaditya@users.noreply.github.com>
2024-02-16 12:18:23 +0530

    Update WithDraw.java

--- spring/demo/src/main/java/com/example/demo/controller/WithDraw.java
+++ spring/demo/src/main/java/com/example/demo/controller/WithDraw.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.config.Bank;
